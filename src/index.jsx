c0af33928cccc892f706672f98e7140d748b568d
Arem58 <arem.9958@gmail.com>
2021-04-19 05:50:12 -0600

    avances

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-// import Boton from './componentes/Boton'
+import Boton from './componentes/Boton'
 import Pantalla from './componentes/Pantalla'
 import './styles/calculator.scss'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -20,22 +17,98 @@ const App = () => {
   return (
     <div className="calculator">
       <Pantalla number={numero} />
-      <Container>
-        <Row>
-          <Col>
-            hola
-          </Col>
-          <Col>
-            hola
-          </Col>
-          <Col>
-            hola
-          </Col>
-          <Col>
-            hola
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        {/* Teclas C, +/-, %, ÷ */}
+        <div className="row g-0">
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="÷" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="X" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="%" />
+            </div>
+          </div>
+        </div>
+        {/* Teclas 7, 8, 9, X */}
+        <div className="row g-0">
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="7" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="8" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="9" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="+" />
+            </div>
+          </div>
+        </div>
+        <div className="row g-0">
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+        </div>
+        <div className="row g-0">
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+          <div className="col d-flex">
+            <div className="espacioBoton">
+              <Boton onClick={changeScreen} title="C" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
